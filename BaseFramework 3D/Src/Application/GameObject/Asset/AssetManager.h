f24c31bdbdfaafd934f482760904909ff8b98b9b
fujiriku3988 <kd1350208@st.kobedenshi.ac.jp>
2024-07-16 09:24:47 +0900

    AssetManagerの制作途中

---
+++ BaseFramework 3D/Src/Application/GameObject/Asset/AssetManager.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+class AssetManager
+{
+public:
+
+	void Init();
+	void LoadAsset(const std::string name, const std::string filepass);
+	
+
+private:
+	std::shared_ptr<KdModelWork>m_model;
+	std::shared_ptr<KdSquarePolygon>m_poly;
+	KdTexture m_tex;
+	std::unordered_map<std::string, std::shared_ptr<KdModelWork>> m_asset;
+	//std::unordered_map<std::string, std::shared_ptr<KdSquarePolygon>> m_asset;
+	//std::unordered_map<std::string, KdTexture> m_asset;
+
+private:
+
+	AssetManager() {}
+	~AssetManager() {}
+
+public:
+
+	// シングルトンパターン
+	// 常に存在する && 必ず1つしか存在しない(1つしか存在出来ない)
+	// どこからでもアクセスが可能で便利だが
+	// 何でもかんでもシングルトンという思考はNG
+	static AssetManager& Instance()
+	{
+		static AssetManager instance;
+		return instance;
+	}
+};
